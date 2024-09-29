759e726174a82e7a629459b516fd863f9f7afab6
dzhma <dzhma@proton.me>
2024-09-29 14:36:18 +0400

    new theme + added emacs config

---
+++ emacs/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("1ed24156ed1ec7087890d46ab5bed842086cdad6969d3a95f09ea3a57f26696d" default))
+ '(package-selected-packages '(org-roam-ui vertico-posframe org-modern org-roam)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- nvim/lua/dzhma/plugins/colorscheme.lua
+++ nvim/lua/dzhma/plugins/colorscheme.lua
@@ -8,12 +8,12 @@ return {
                     return {
                         ["@markup.strong"] = { fg = "#FF0000", bold = true },
                         ["@markup.italic"] = { fg = "#FFFF00", italic = true },
-                        ["@markup.heading.1.markdown"] = { fg = "#FF0000", bg = "#400000", bold = true, italic = true },  -- Gold
-                        ["@markup.heading.2.markdown"] = { fg = "#FFD700", bg = "#5B4A08", bold = true, italic = true },  -- Gold
-                        ["@markup.heading.3.markdown"] = { fg = "#7CFC00", bg = "#013220", bold = true, italic = true },  -- LawnGreen
-                        ["@markup.heading.4.markdown"] = { fg = "#40E0D0", bg = "#065465", bold = true, italic = true },  -- Turquoise
-                        ["@markup.heading.5.markdown"] = { fg = "#FF8C00", bg = "#B03608", bold = true, italic = true },  -- DarkOrange
-                        ["@markup.heading.6.markdown"] = { fg = "#9370DB", bg = "#301934", bold = true, italic = true},   -- MediumPurple
+                        ["@markup.heading.1.markdown"] = { fg = "#FF0000", bg = "#400000", bold = true },  -- Gold ["@markup.heading.2.markdown"] = { fg = "#FFD700", bg = "#5B4A08", bold = true, italic = true },  -- Gold
+                        ["@markup.heading.2.markdown"] = { fg = "#FFD700", bg = "#5B4A08", bold = true },  -- Gold
+                        ["@markup.heading.3.markdown"] = { fg = "#7CFC00", bg = "#013220", bold = true },  -- LawnGreen
+                        ["@markup.heading.4.markdown"] = { fg = "#40E0D0", bg = "#065465", bold = true },  -- Turquoise
+                        ["@markup.heading.5.markdown"] = { fg = "#FF8C00", bg = "#B03608", bold = true },  -- DarkOrange
+                        ["@markup.heading.6.markdown"] = { fg = "#9370DB", bg = "#301934", bold = true },   -- MediumPurple
                         ["@variable"] = { fg = "#41FDFE", bold = true },
                         ["Structure"] = { fg = colors.dark_cyan },
                         ["Function"] = { fg = "#0ADD08", underline = true },
@@ -26,11 +26,12 @@ return {
                         ["GitSignsTopdelete"] = { fg = "#FF0000" },
                         ["GitSignsChangedelete"] = { fg = "#FF0000" },
                         ["GitSignsUntracked"] = { fg = "#FF0000" },
+                        ["@markup.math.latex"] = { fg = "#E31C79" },
                     }
                 end,
             })
 
             vim.cmd.colorscheme('dark_flat')
         end,
-    }
+    },
 }
