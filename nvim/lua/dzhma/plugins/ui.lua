2abb0b1ca9b11ab6e9b6f22025c45d2e519277a7
dzhma <dzhma@proton.me>
2024-11-25 20:16:35 +0400

    added cursor animations to kitty (wow)

--- nvim/lua/dzhma/plugins/ui.lua
+++ nvim/lua/dzhma/plugins/ui.lua
@@ -51,39 +51,6 @@ return {
     },
 
     {
-        "nvim-tree/nvim-tree.lua",
-        keys = {
-            { "<A-t>", "<cmd>NvimTreeToggle<CR>", desc = "Open Nvim Tree" },
-        },
-        config = function ()
-            vim.g.loaded_netrw = 1
-            vim.g.loaded_netrwPlugin = 1
-
-            -- optionally enable 24-bit colour
-            vim.opt.termguicolors = true
-
-            -- empty setup using defaults
-            require("nvim-tree").setup()
-
-            -- OR setup with some options
-            require("nvim-tree").setup({
-                sort = {
-                    sorter = "case_sensitive",
-                },
-                view = {
-                    width = 30,
-                },
-                renderer = {
-                    group_empty = true,
-                },
-                filters = {
-                    dotfiles = true,
-                },
-            })
-        end
-    },
-
-    {
         -- Git information integration
         'lewis6991/gitsigns.nvim',
         event = "VeryLazy",
