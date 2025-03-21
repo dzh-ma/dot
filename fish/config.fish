6bfce079eee255a67c1aa517265de05bc1631905
dzh-ma <dzhma@proton.me>
2025-03-21 20:19:24 +0400

    Added fish aliases

--- fish/config.fish
+++ fish/config.fish
@@ -20,7 +20,6 @@ if status is-interactive
     alias ckr="clockify-cli report"
     alias cko="clockify-cli out"
     alias power="upower -i /org/freedesktop/UPower/devices/battery_BAT0"
-    alias pp="powerprofilesctl"
     alias mix="pulsemixer"
     alias nmwc="sudo nmcli dev wifi connect"
     alias t="tmux"
@@ -44,8 +43,6 @@ if status is-interactive
     alias gs="git push"
     alias gl="git pull"
     alias q="exit"
-    alias fs="fan_state get"
-    alias fS="fan_state set"        # 0-3 = standard / quiet / high / full
     alias a="clear"
     alias b="btop"
     alias B="bluetui"
@@ -56,6 +53,12 @@ if status is-interactive
     alias d0="hyprctl keyword monitor 'eDP-1, disable'"
     alias d1="hyprctl keyword monitor 'eDP-1, 2880x1800@120, 0x0, 1'"
     alias l="libreoffice"
+    alias fs="fan_state get"
+    alias fS="fan_state set"        # 0-3 = standard / quiet / high / full
+    alias pp="powerprofilesctl"
+    alias battery="fan_state set 1 && powerprofilesctl set power-saver"
+    alias balance="fan_state set 0 && powerprofilesctl set balanced"
+    alias performance="fan_state set 3 && powerprofilesctl set performance"
 
     # ENVIRONMENT VARIABLES
     set -x PATH $PATH $HOME/.local/bin
