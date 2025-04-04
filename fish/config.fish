adeea9b7b685913ec76b5c1f38e2a2a274876997
dzh-ma <dzhma@proton.me>
2025-04-04 19:12:43 +0400

    Added fish alias

--- fish/config.fish
+++ fish/config.fish
@@ -61,6 +61,7 @@ if status is-interactive
     alias performance="fan_state set 3 && powerprofilesctl set performance"
     alias pac="sudo pacman"
     alias hwifi="nmcli connection up Bedroom\ 5GHz"
+    alias uwifi="nmcli connection up eduroam\ \[a16c8929\]"
     alias jumd="jupytext --to markdown"
     alias juno="jupytext --to notebook"
 
