eb649d668838742e90c838b2c70515055bf2a995
dzh-ma <dzhma@proton.me>
2025-04-10 21:57:45 +0400

    Installed snacks.nvim

--- nvim/lua/dzhma/plugins/function.lua
+++ nvim/lua/dzhma/plugins/function.lua
@@ -275,4 +275,41 @@ return {
         "wakatime/vim-wakatime",
         lazy = false,
     },
+
+    -- DOCS: Git manager
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        keys = {
+            { "<Leader>g", "<cmd>LazyGit<CR>", desc = "LazyGit" },
+        },
+    },
+
+    -- DOCS: QoL improvements
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        ---@type snacks.Config
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+            bigfile = { enabled = true },
+            dashboard = { enabled = true },
+            indent = { enabled = true },
+            input = { enabled = true },
+            words = { enabled = true },
+        },
+    }
 }
