4dd88cfdc702c59dab473603baffdab65dc3484f
dzh-ma <dzhma@proton.me>
2025-04-10 10:28:22 +0400

    Revert back to lazygit from neogit

--- nvim/lua/dzhma/plugins/git.lua
+++ nvim/lua/dzhma/plugins/git.lua
@@ -1,30 +1,19 @@
 return {
-    -- {
-    --     "kdheepak/lazygit.nvim",
-    --     lazy = true,
-    --     cmd = {
-    --         "LazyGit",
-    --         "LazyGitConfig",
-    --         "LazyGitCurrentFile",
-    --         "LazyGitFilter",
-    --         "LazyGitFilterCurrentFile",
-    --     },
-    --     dependencies = {
-    --         "nvim-lua/plenary.nvim",
-    --     },
-    --     keys = {
-    --         { "<Leader>g", "<cmd>LazyGit<CR>", desc = "LazyGit" },
-    --     },
-    -- },
     {
-        "NeogitOrg/neogit",
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
         dependencies = {
-            "nvim-lua/plenary.nvim",         -- required
-            "sindrets/diffview.nvim",        -- optional - Diff integration
-            "nvim-telescope/telescope.nvim", -- optional
+            "nvim-lua/plenary.nvim",
         },
         keys = {
-            { "<Leader>g", "<cmd>lua require('neogit').open()<CR>" }
-        }
+            { "<Leader>g", "<cmd>LazyGit<CR>", desc = "LazyGit" },
+        },
     },
 }
