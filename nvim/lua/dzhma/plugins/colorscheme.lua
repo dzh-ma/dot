8f4a2e64898f26bd842def0299f62b6179aa89f6
dzh-ma <dzhma@proton.me>
2025-04-12 20:39:09 +0400

    Neorg color changes

--- nvim/lua/dzhma/plugins/colorscheme.lua
+++ nvim/lua/dzhma/plugins/colorscheme.lua
@@ -13,21 +13,31 @@ return {
                 borderless_pickers = true,
                 overrides = function (colors)
                     return {
+                        Comment = { fg = colors.bg_highlight, italic = true },
+
                         -- Neorg markup
                         ["@neorg.markup.bold.norg"] = { fg = colors.yellow, bold = true },
-                        ["@neorg.markup.underline.norg"] = { fg = colors.magenta, underline = true },
+                        -- ["@neorg.markup.underline.norg"] = { fg = colors.magenta, underline = true },
                         ["@neorg.markup.variable.norg"] = { fg = colors.cyan , italic = true },
                         ["@neorg.markup.superscript.norg"] = { fg = colors.green },
                         ["@neorg.markup.subscript.norg"] = { fg = colors.red },
-                        ["@neorg.markup.verbatim.norg"] = { bg = colors.bg_highlight },
-                        ["@neorg.definitions.content.norg"] = { fg = colors.pink, italic = true },
+                        ["@neorg.markup.verbatim.norg"] = { bg = "#c7c7c7" },
+
+                        -- Neorg footnotes
+                        ["@neorg.footnotes.title.norg"] = { fg = colors.cyan, bold = true, italic = true },
                         ["@neorg.footnotes.content.norg"] = { fg = colors.purple, italic = true },
+                        ["@neorg.links.location.footnote.norg"] = { fg = colors.cyan, bold = true, italic = true },
+
+                        -- Neorg definitions
+                        ["@neorg.definitions.title.norg"] = { fg = colors.yellow, bold = true, italic = true },
+                        ["@neorg.definitions.content.norg"] = { fg = colors.pink, italic = true },
+                        ["@neorg.links.location.definition.norg"] = { fg = colors.yellow, bold = true, italic = true },
 
                         -- Neorg metadata
                         ["@neorg.tags.ranged_verbatim.document_meta.title.norg_meta"] = { bold = true, underline = true },
                         ["@neorg.tags.ranged_verbatim.document_meta.key.norg_meta"] = { fg = colors.grey, bold = true },
 
-                        -- Neorg header titles
+                        -- Neorg link header titles
                         ["@neorg.links.location.heading.1.norg"] = { fg = colors.bg_alt, bg = colors.red, bold = true, italic = true },
                         ["@neorg.links.location.heading.2.norg"] = { fg = colors.bg_alt, bg = colors.yellow, bold = true, italic = true },
                         ["@neorg.links.location.heading.3.norg"] = { fg = colors.bg_alt, bg = colors.cyan, bold = true, italic = true },
@@ -35,7 +45,7 @@ return {
                         ["@neorg.links.location.heading.5.norg"] = { fg = colors.bg_alt, bg = colors.orange, bold = true, italic = true },
                         ["@neorg.links.location.heading.6.norg"] = { fg = colors.bg_alt, bg = colors.green, bold = true, italic = true },
 
-                        -- Neorg header prefix
+                        -- Neorg link header prefix
                         ["@neorg.links.location.heading.1.prefix.norg"] = { fg = colors.bg_alt, bg = colors.red, bold = true, italic = true },
                         ["@neorg.links.location.heading.2.prefix.norg"] = { fg = colors.bg_alt, bg = colors.yellow, bold = true, italic = true },
                         ["@neorg.links.location.heading.3.prefix.norg"] = { fg = colors.bg_alt, bg = colors.cyan, bold = true, italic = true },
