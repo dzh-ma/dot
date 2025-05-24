75bf5a81dca8ab70112d0678de1fc1e1bec16d08
dzh-ma <dzhma@proton.me>
2025-05-24 13:48:02 +0400

    config updagte

--- nvim/lua/dzhma/plugins/function.lua
+++ nvim/lua/dzhma/plugins/function.lua
@@ -160,7 +160,7 @@ return {
     -- DOCS: Automatic pairing
     {
         "altermo/ultimate-autopair.nvim",
-        event={"InsertEnter","CmdlineEnter"},
+        event={ "InsertEnter","CmdlineEnter" },
         branch="v0.6", --recommended as each new version will have breaking changes
         opts = {},
     },
